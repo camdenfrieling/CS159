3e35587ea00a5d0580edf85a103befd86873820c
Adrian Chen <33459725+adrianchen8662@users.noreply.github.com>
2020-02-10 21:38:07 -0500

    Update lab03.c

--- lab03.c
+++ lab03.c
@@ -1,5 +1,26 @@
-include<stdio.h>
-include<math.h>
+
+/***************************************************************************
+ *  Programmers and Purdue Email Addresses:
+ *  1. chen3124@purdue.edu
+ *  2. chu171@purdue.edu
+ *  3. cfrielin@purdue.edu
+ *
+ *  Lab #: 03
+ *
+ *  Academic Integrity Statement:
+ *
+ *  We have not used source code obtained from any other unauthorized source,
+ *  either modified or unmodified.  Neither have we provided access to our code
+ *  to another. The effort we are submitting is our own original work.
+ *
+ *  Day, Time, Location of Lab: Tuesday, 9:30am , SC 189
+ *
+ *  Program Description: Finds the force of tension, mass per unit of length, velocity of wave generated and time to travel string length from length of string, mass of block maintaining tension, and either string mass or velocity of wave.
+ *
+ ***************************************************************************/
+
+#include<stdio.h>
+#include<math.h>
 #define GRAVITY 9.8 //gravitational constant(meters per second squared)
 
 int main()
@@ -11,6 +32,11 @@ int main()
   double selectionnumber = 0; //value from selection number(kg or meters per second)
   double massString = 0; //mass of the string(kg)
   double velocity = 0; //velocity(meters per second)
+  double force = 0; //force of tension(N)
+  double massPerUnit = 0; //mass per unit of length (kg/m)
+  double time = 0; //time to travel string length (s)
+  double mu1 = 0; //first possible mass per unit of length value
+  double mu2 = 0; //second possible mass per unit of length value
 
   //asks and gets values for length of string and mass of block
   printf("Enter length of string (m) -> ");
@@ -27,23 +53,39 @@ int main()
 
   //subtracts 1 from selection to get either 0(String mass) or 1(Velocity of wave)
   selection--;
+  //separates mass of string and velocity
   massString = selectionnumber * (1 - selection);
   velocity = selectionnumber * selection;
 
+  //calculates force
   force = massBlock * GRAVITY;
 
-  mu1 = (force / pow(velocity,2)) * selection;
+  //calculates two possible mass per unit of length from different equations
+  mu1 = (force / ((velocity * velocity) + (1 - selection))) * selection;
   mu2 = (massString / lengthString) * (1 - selection);
+
+  //calculates the mass per unit and compensates for divide by zero protection above
+  mu1 = mu1 * selection;
   massPerUnit = mu1 + mu2;
+
+  //calculates the mass of the string
+  massString = (selectionnumber * (1 - selection)) + (lengthString * massPerUnit * (selection));
+  //calculates the velocity of the wave generated
+  velocity = (selectionnumber * selection) + (sqrt(force / massPerUnit)) * (1 - selection);
+
+  //calculates the time to travel string length
+  time = lengthString / velocity;
 
   printf("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
   printf("\nMass of string:%11.1lf (kg)",massString);
-  printf("\nLength of string:%9.1lf(m)",lengthString);
-  printf("\nMass of block:%11.1lf",massBlock);
+  printf("\nLength of string:%9.1lf (m)",lengthString);
+  printf("\nMass of block:%11.1lf (kg)",massBlock);
   printf("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
-  printf("\nForce of tension:%11.1lf",force);
-  printf("\nMass per unit of length:%11.3lf,massPerUnit");
-  printf("\nVelocity of wave generated:%11.1lf (kg/m)",velocity);
-  printf("\nTime to travel string length:%9.3lf", time);
+  printf("\nForce of tension:%11.1lf (N)",force);
+  printf("\nMass per unit of length:%11.3lf (kg/m)",massPerUnit);
+  printf("\nVelocity of wave generated:%11.1lf (m/s)",velocity);
+  printf("\nTime to travel string length:%9.3lf (s)", time);
   printf("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+
+  return (0);
 }
