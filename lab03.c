6b50a652c1d4dc587d879d544fd5a17b611ef56c
Adrian Chen <33459725+adrianchen8662@users.noreply.github.com>
2020-02-10 22:16:31 -0500

    Update lab03.c

--- lab03.c
+++ lab03.c
@@ -19,27 +19,6 @@
  *
  ***************************************************************************/
 
-
-/***************************************************************************
-*
-*  Programmers and Purdue Email Addresses:
-*  1. login1@purdue.edu
-*  2. login2@purdue.edu
-*  3. login3@purdue.edu (delete line if no third partner)
-*
-*  Lab #:
-*
-*  Academic Integrity Statement:
-*
-*  We have not used source code obtained from any other unauthorized source,
-*  either modified or unmodified.  Neither have we provided access to our code
-*  to another. The effort we are submitting is our own original work.
-*
-*  Day, Time, Location of Lab:
-*
-*  Program Description:
-*
-***************************************************************************/
 #include<stdio.h>
 #include<math.h>
 #define GRAVITY 9.8 //gravitational constant(meters per second squared)
@@ -95,7 +74,7 @@ int main()
   velocity = (selectionnumber * selection) + (sqrt(force / massPerUnit)) * (1 - selection);
 
   //calculates the time to travel string length
-  time = lengthString / velocity;
+  time = (lengthString-1) / velocity;
 
   printf("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
   printf("\nMass of string:%11.1lf (kg)",massString);
