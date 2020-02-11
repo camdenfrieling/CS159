0b64a469113004ec722ec375fcaf3d0b390e7e29
Adrian Chen <33459725+adrianchen8662@users.noreply.github.com>
2020-02-10 21:20:37 -0500

    Update lab03.c

--- lab03.c
+++ lab03.c
@@ -1,85 +1,49 @@
-
-/***************************************************************************
-*  Programmers and Purdue Email Addresses:
-*  1. chen3124@purdue.edu
-*  2. chu171@purdue.edu
-*  3. cfrielin@purdue.edu
-*
-*  Lab #: 03
-*
-*  Academic Integrity Statement:
-*
-*  We have not used source code obtained from any other unauthorized source,
-*  either modified or unmodified.  Neither have we provided access to our code
-*  to another. The effort we are submitting is our own original work.
-*
-*  Day, Time, Location of Lab: Tuesday, 9:30am , SC 189
-*
-*  Program Description: Finds the force of tension, mass per unit of length, velocity of wave generated and time to travel string length from length of string, mass of block maintaining tension, and either string mass or velocity of wave.
-*
-***************************************************************************/
-
-#include<stdio.h>
-#include<math.h>
-#define GRAVITY 9.8
+include<stdio.h>
+include<math.h>
+#define GRAVITY 9.8 //gravitational constant(meters per second squared)
 
 int main()
 {
   //variable declaration
-  double massStr = 0; //mass of the string(kg)
-  double lenStr = 0; //length of the string(meter)
-  double massBlock = 0; //mass of the block(kg)
-  double force = 0; //force the block applies(N)
-  double mspul = 0; //mass of string per unit length(kg/m)
-  double mspulOne = 0;
-  double mspulTwo = 0;
-  double velocity = 0; //velocity of the wave generated(m/s)
-  double time = 0; //time it takes for the wave to travel the length of the pulley(s)
+  double lengthString = 0; //length of string(meter)
+  double massBlock = 0; //mass of block(kg)
   double selection = 0; //selection number
-  double selectionnumber = 0; //value of selection number
-  double max = 0;
+  double selectionnumber = 0; //value from selection number(kg or meters per second)
+  double massString = 0; //mass of the string(kg)
+  double velocity = 0; //velocity(meters per second)
 
-
-  //allows user to enter values for length of string and mass of block
+  //asks and gets values for length of string and mass of block
   printf("Enter length of string (m) -> ");
-  scanf("%lf",&lenStr);
+  scanf("%lf",&lengthString);
   printf("Enter mass of block maintaining tension (kg) -> ");
   scanf("%lf",&massBlock);
 
-  printf("\n1. Enter string mass\n2. Enter velocity of wave");
-
-  printf("\n\nMake your selection -> ");
+  //asks and gets values for selection number and value gained from selection number
+  printf("\n1. Enter string mass\n2. Enter velocity of wave\n");
+  printf("\nMake your selection -> ");
   scanf("%lf",&selection);
-  printf("Enter value for selection -> ");
+  printf("\nEnter value for selection -> ");
   scanf("%lf",&selectionnumber);
 
+  //subtracts 1 from selection to get either 0(String mass) or 1(Velocity of wave)
   selection--;
-  massStr = selectionnumber;
-  velocity = selectionnumber;
+  massString = selectionnumber * (1 - selection);
+  velocity = selectionnumber * selection;
 
-  force = massBlock *  GRAVITY;
+  force = massBlock * GRAVITY;
 
-  mspulOne = (force / pow(velocity,2)) * selection;
-  mspulTwo = (massStr / lenStr) * (1 - selection);
-  mspulOne++;
-  mspulTwo++;
+  mu1 = (force / pow(velocity,2)) * selection;
+  mu2 = (massString / lengthString) * (1 - selection);
+  massPerUnit = mu1 + mu2;
 
-  max = mspulOne / mspulTwo * mspulOne + mspulTwo / mspulOne * mspulTwo;
-  max--;
-  printf("\nmspulOne: %.3lf \nmspulTwo: %.3lf", mspulOne, mspulTwo);
-  printf("max %.3lf", max);
-
-  massStr = lenStr * max;
-  velocity = sqrt(force / max);
-
-
-  printf("\n\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
-  printf("\nMass of string:%11.1lf (kg)",massStr);
-  printf("\nLength of string:%9.1lf  (m)",lenStr);
+  printf("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+  printf("\nMass of string:%11.1lf (kg)",massString);
+  printf("\nLength of string:%9.1lf(m)",lengthString);
   printf("\nMass of block:%11.1lf",massBlock);
+  printf("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
   printf("\nForce of tension:%11.1lf",force);
-  printf("\nMass per unit of length:%11.3lf",max);
+  printf("\nMass per unit of length:%11.3lf,massPerUnit");
   printf("\nVelocity of wave generated:%11.1lf (kg/m)",velocity);
   printf("\nTime to travel string length:%9.3lf", time);
+  printf("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
 }
-
